1caa63d83e110d96f193caf3d9cfe820f889207c
matak <matak>
2018-06-30 16:17:09 +0300

    Quick fix

---
+++ public/.module-cache/d36332f29d5379b93036f74e38dd996b2d97cb21.js
@@ -0,0 +1,16 @@
+var ExampleApplication = React.createClass({displayName: "ExampleApplication",
+render: function() {
+var elapsed = Math.round(this.props.elapsed / 100);
+var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
+var message =
+'React ' + seconds + ' saniyedir başarıyla çalışıyor.';
+return React.createElement("p", null, message);
+}
+});
+var start = new Date().getTime();
+setInterval(function() {
+React.render(
+React.createElement(ExampleApplication, {elapsed: new Date().getTime() - start}),
+document.getElementById('container')
+);
+}, 50);
---
+++ src/main.js
@@ -0,0 +1,16 @@
+var ExampleApplication = React.createClass({
+render: function() {
+var elapsed = Math.round(this.props.elapsed / 100);
+var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
+var message =
+'React ' + seconds + ' saniyedir başarıyla çalışıyor.';
+return <p>{message}</p>;
+}
+});
+var start = new Date().getTime();
+setInterval(function() {
+React.render(
+<ExampleApplication elapsed={new Date().getTime() - start} />,
+document.getElementById('container')
+);
+}, 50);
